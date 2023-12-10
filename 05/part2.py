7d784ba8e828b3d2c6f2cc7713ae58b112547e29
Henrik Drefs <henrik.drefs@outlook.de>
2023-12-10 20:19:53 +0100

    day 5 wip

---
+++ 05/part2.py
@@ -0,0 +1,43 @@
+import re
+with open('input.txt' ) as f:
+  seeds = next(f)
+  m = re.match(r'seeds:((?: \d+ \d+)+)', seeds)
+  seeds = (int(s) for s in m.group(1).split())
+  seeds = zip(seeds, seeds)
+  t = 0
+  for si, sr in seeds:
+    t += sr
+    print(sr)
+  print(t)
+  exit()
+  current_map = None
+  maps = {}
+  for line in f:
+    line = line.strip()
+    if not line:
+      current_map = None
+      continue
+    if not current_map:
+      m = re.match(r'([a-z]+)-to-([a-z]+) map:', line)
+      src, dest = m.groups()
+      current_map = (src, dest)
+      maps[src] = (dest, [])
+      continue
+    dest_range_start, src_range_start, range_len = [int(s) for s in line.split()]
+    maps[current_map[0]][1].append((dest_range_start, src_range_start, range_len))
+
+def convert_value(value: int, src_category: str, dest_category: str):
+  target_category, ranges = maps[src_category]
+  for dest_range_start, src_range_start, range_len in ranges:
+    if value >= src_range_start and value < src_range_start + range_len:
+      converted = value - src_range_start + dest_range_start
+      break
+  else:
+    converted = value
+  if target_category == dest_category:
+    return converted
+  return convert_value(converted, target_category, dest_category)
+
+min_location = min( convert_value(seed_start + offset, 'seed', 'location')  for seed_start, seed_range in seeds for offset in range(seed_range))
+
+print(min_location)
